5e927a5bbe3daba6c8e101d7975c140f222d2c5d
Uzair Aftab <uzaaft@outlook.com>
2023-03-04 17:25:24 +0100

    feat(heimdall): Basic event loop with parsing of config.

--- heimdall/src/main.rs
+++ heimdall/src/main.rs
@@ -1,3 +1,52 @@
+// Copyright 2022-2022 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+mod config;
+
+use std::str::FromStr;
+
+use global_hotkey::{
+    hotkey::{Code, HotKey, Modifiers},
+    GlobalHotKeyEvent, GlobalHotKeyManager,
+};
+use winit::event_loop::{ControlFlow, EventLoopBuilder};
+
+// Read config file from XDG_CONFIG_HOME. Fallback to ~/.config/heimdall/config.toml
+fn read_config() -> config::Config {
+    // This can probably be done better
+    let config_path = std::env::var("XDG_CONFIG_HOME")
+        .unwrap_or_else(|_| format!("{}/.config", std::env::var("HOME").unwrap()))
+        + "/heimdall/config.toml";
+    let config_file = std::fs::read_to_string(config_path).unwrap_or(
+        // Just a default config for now
+        r#"
+        [[bindings]]
+        key = "C"
+        modifiers = ["Ctrl", "Shift"]
+        command = "echo hello"
+        [[bindings]]
+        key = "D"
+        modifiers = ["Ctrl"]
+        command = "echo hello"
+        "#
+        .to_string(),
+    );
+    config::Config::from_str(&config_file).unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoopBuilder::new().build();
+    let config = read_config();
+
+    let hotkeys_manager = GlobalHotKeyManager::new().unwrap();
+
+    let global_hotkey_channel = GlobalHotKeyEvent::receiver();
+
+    event_loop.run(move |_event, _, control_flow| {
+        // Just print registered hotkeys for now
+        if let Ok(event) = global_hotkey_channel.try_recv() {
+            println!("{event:?}");
+        }
+    })
 }
