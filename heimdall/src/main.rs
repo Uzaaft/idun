b85bae41cf6c6c28312567fd7e7904cbe99f763a
Uzair Aftab <uzaaft@outlook.com>
2023-03-05 10:32:58 +0100

    feat(heimdall): Load config from user on run, and split up code more. Use hashmap to save keys and commands

--- heimdall/src/main.rs
+++ heimdall/src/main.rs
@@ -4,8 +4,9 @@
 
 mod config;
 
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
+use anyhow::Result;
 use global_hotkey::{
     hotkey::{Code, HotKey, Modifiers},
     GlobalHotKeyEvent, GlobalHotKeyManager,
@@ -13,40 +14,41 @@ use global_hotkey::{
 use winit::event_loop::{ControlFlow, EventLoopBuilder};
 
 // Read config file from XDG_CONFIG_HOME. Fallback to ~/.config/heimdall/config.toml
-fn read_config() -> config::Config {
-    // This can probably be done better
+// TODO: Add better error handling
+fn read_config() -> Result<config::Config> {
     let config_path = std::env::var("XDG_CONFIG_HOME")
         .unwrap_or_else(|_| format!("{}/.config", std::env::var("HOME").unwrap()))
         + "/heimdall/config.toml";
-    let config_file = std::fs::read_to_string(config_path).unwrap_or(
-        // Just a default config for now
-        r#"
-        [[bindings]]
-        key = "C"
-        modifiers = ["Ctrl", "Shift"]
-        command = "echo hello"
-        [[bindings]]
-        key = "D"
-        modifiers = ["Ctrl"]
-        command = "echo hello"
-        "#
-        .to_string(),
-    );
-    config::Config::from_str(&config_file).unwrap()
+    let config_file = std::fs::read_to_string(config_path)?.to_string();
+    Ok(config::Config::from_str(&config_file)?)
 }
 
-fn main() {
-    let event_loop = EventLoopBuilder::new().build();
-    let config = read_config();
-
+fn register_keys() -> Result<(GlobalHotKeyManager, HashMap<HotKey, String>)> {
+    let user_config = read_config()?;
     let hotkeys_manager = GlobalHotKeyManager::new().unwrap();
+    let key_command_map: HashMap<HotKey, String> = user_config
+        .bindings
+        .iter()
+        .map(|hotkey| {
+            let key: HotKey = hotkey.to_string().parse().unwrap();
+            hotkeys_manager.register(key).unwrap();
+            (key, hotkey.command.to_string())
+        })
+        .collect();
+    Ok((hotkeys_manager, key_command_map))
+}
+
+fn main() -> Result<()> {
+    let event_loop = EventLoopBuilder::new().build();
 
     let global_hotkey_channel = GlobalHotKeyEvent::receiver();
+    let (hotkeys_manager, key_command_map) = register_keys()?;
 
     event_loop.run(move |_event, _, control_flow| {
         // Just print registered hotkeys for now
         if let Ok(event) = global_hotkey_channel.try_recv() {
             println!("{event:?}");
         }
-    })
+    });
+    Ok(())
 }
